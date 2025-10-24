d6ef5b03b52eb2a6708a56ff2bf11a146aadbc76
SARUTOBI10 <mt6055365@gmail.com>
2025-10-24 14:26:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("mySidebar").style.width="100vw";
+    document.querySelector("body").style.overflow="hidden";
+}
+
+function closeNav(){
+    document.getElementById("mySidebar").style.width="0";
+    document.querySelector("body").style.overflow="auto";
+}
